d78fc23a705cc916f6b577097540a8b40b22b3ab
Pilar <pilarchia.14@gmail.com>
2025-04-15 23:15:11 -0400

    Added recent projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,46 +1,36 @@
 import {Container, Row, Col, Tab, Nav} from 'react-bootstrap'
 import {ProjectCard} from "./ProjectCard"
 import SproutScript from "../images/ProjectsImages-resized/SproutScript.jpg"
-import DoubleSpace from "../images/ProjectsImages-resized/DoubleSpace.jpg"
-import Shell from "../images/ProjectsImages-resized/Shell.jpg"
-import PersonalWebsite from "../images/ProjectsImages-resized/PersonalWebsite.jpg"
 import Checkout from "../images/ProjectsImages-resized/Checkout.jpg"
 
 export const Projects = () => {
-    const extracurricularProjects = [
+    const apiProjects = [
         {
-            title: "Sprout Script",
+            title: "API Design and Development (Python, FASTAPI, Pytest)",
+            description: "Engineered an API for text storage and URL shortening.",
+            imgUrl: SproutScript,
+            link: "https://ex01-comp590-140-25sp-pchiaq.apps.unc.edu/docs",
+        },
+        {
+            title: "Frontend Design (Angular and TypeScript)",
+            description: "Built a front-end for API interaction.",
+            imgUrl: SproutScript,
+            link: "https://ex01-comp590-140-25sp-pchiaq.apps.unc.edu/docs",
+        },
+    ];
+
+    const gameProjects = [
+        {
+            title: "Sprout Script (Python, Pygame)",
             description: "Young players develop coding skills as they nurture and care for a digital plant.",
             imgUrl: SproutScript,
             link: "https://devpost.com/software/pearl-hacks-24",
         },
         {
-            title: "Double Space",
-            description: "An award winning, story-driven, action packed, adventure game created for a game jam.",
-            imgUrl: DoubleSpace,
-            link: "https://chamina.itch.io/double-space",
-        },
-    ];
-
-    const personalProjects = [
-        {
-            title: "Check-out Challenge",
+            title: "Check-out Challenge (Unity, C#)",
             description: "An immersive gaming experience created in my free time to improve my coding and design skills.",
             imgUrl: Checkout,
             link: "https://mchia157.github.io/CheckoutChallenge",
-        },
-        {
-            title: "Personal Website",
-            description: "The website you are navigating was made with React and React Bootstrap.",
-            imgUrl: PersonalWebsite,
-        },
-    ]
-
-    const schoolProjects = [
-        {
-            title: "A Shell in a Linux Environment",
-            description: "The shell performs operations like changing directories and executing system programs such as pwd and ls.",
-            imgUrl: Shell,
         },
     ]
     return (
@@ -52,20 +42,20 @@ export const Projects = () => {
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                             <Nav.Item>
-                                <Nav.Link eventKey="first">Extracurricular</Nav.Link>
+                                <Nav.Link eventKey="first">API & Frontend</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="second">Personal</Nav.Link>
+                                <Nav.Link eventKey="second">Java MVC Game</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="third">School</Nav.Link>
+                                <Nav.Link eventKey="third">Other Games</Nav.Link>
                             </Nav.Item>
                         </Nav>
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
                                 <Row className="justify-content-center">
                                     {
-                                        extracurricularProjects.map((project, index) => {
+                                        apiProjects.map((project, index) => {
                                             return (
                                                 <ProjectCard
                                                 key={index}
@@ -76,24 +66,10 @@ export const Projects = () => {
                                     }
                                 </Row>
                             </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                                <Row className="justify-content-center">
-                                    {
-                                        personalProjects.map((project, index) => {
-                                            return (
-                                                <ProjectCard
-                                                key={index}
-                                                {...project}
-                                                />
-                                            )
-                                        }) 
-                                    }
-                                </Row>
-                            </Tab.Pane>
                             <Tab.Pane eventKey="third">
                                 <Row className="justify-content-center">
                                     {
-                                        schoolProjects.map((project, index) => {
+                                        gameProjects.map((project, index) => {
                                             return (
                                                 <ProjectCard
                                                 key={index}
